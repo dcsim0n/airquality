11430c1b64a9edf6abd6a5a90cd49ef196dca31b
Dana Simmons <dsanalog@gmail.com>
2021-09-08 10:53:12 -0400

    Create basic air quality snesor
    Why: Sensor should have text and graphic displays while reading data
    from PMS sensor
    
    *---
    
    How: basic event loop that reads data and writes to LCD / LED bar graph
    
    *---

---
+++ airquality.ino
@@ -0,0 +1,137 @@
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
+
+#include <Wire.h>
+
+/* Test sketch for Adafruit PM2.5 sensor with UART or I2C */
+
+#include "Adafruit_PM25AQI.h"
+
+// If your PM2.5 is UART only, for UNO and others (without hardware serial) 
+// we must use software serial...
+// pin #2 is IN from sensor (TX pin on sensor), leave pin #3 disconnected
+// comment these two lines if using hardware serial
+//#include <SoftwareSerial.h>
+//SoftwareSerial pmSerial(2, 3);
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
+
+void setup() {
+  // Wait for serial monitor to open
+  Serial.begin(115200);
+//  while (!Serial) delay(10);
+
+  Serial.println("Adafruit PMSA003I Air Quality Sensor");
+
+  // Wait one second for sensor to boot up!
+  delay(1000);
+
+  // If using serial, initialize it and set baudrate before starting!
+  // Uncomment one of the following
+  //Serial1.begin(9600);
+  //pmSerial.begin(9600);
+
+  // There are 3 options for connectivity!
+  if (! aqi.begin_I2C()) {      // connect to the sensor over I2C
+  //if (! aqi.begin_UART(&Serial1)) { // connect to the sensor over hardware serial
+  //if (! aqi.begin_UART(&pmSerial)) { // connect to the sensor over software serial 
+    Serial.println("Could not find PM 2.5 sensor!");
+    while (1) delay(10);
+  }
+
+  Serial.println("PM25 found!");
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+    display.display();
+  delay(2000); // Pause for 2 seconds
+
+    // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+
+
+}
+
+void loop() {
+  PM25_AQI_Data data;
+  
+  if (! aqi.read(&data)) {
+    Serial.println("Could not read from AQI");
+    delay(500);  // try again in a bit!
+    return;
+  }
+  Serial.println("AQI reading success");
+
+  Serial.println();
+  Serial.println(F("---------------------------------------"));
+  Serial.println(F("Concentration Units (standard)"));
+  Serial.println(F("---------------------------------------"));
+  Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_standard);
+  Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_standard);
+  Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_standard);
+  Serial.println(F("Concentration Units (environmental)"));
+  Serial.println(F("---------------------------------------"));
+  Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_env);
+  Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_env);
+  Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_env);
+  Serial.println(F("---------------------------------------"));
+  Serial.print(F("Particles > 0.3um / 0.1L air:")); Serial.println(data.particles_03um);
+  Serial.print(F("Particles > 0.5um / 0.1L air:")); Serial.println(data.particles_05um);
+  Serial.print(F("Particles > 1.0um / 0.1L air:")); Serial.println(data.particles_10um);
+  Serial.print(F("Particles > 2.5um / 0.1L air:")); Serial.println(data.particles_25um);
+  Serial.print(F("Particles > 5.0um / 0.1L air:")); Serial.println(data.particles_50um);
+  Serial.print(F("Particles > 10 um / 0.1L air:")); Serial.println(data.particles_100um);
+  Serial.println(F("---------------------------------------"));
+  
+
+  display.clearDisplay();
+
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  // Not all the characters will fit on the display. This is normal.
+  // Library will draw what it can and the rest will be clipped.
+  char buff[20];
+  memset(buff,0,20);
+  sprintf(buff, "PM10:%d\n", data.pm100_env);
+  display.write(buff);
+  memset(buff,0,20);
+  sprintf(buff, "PM2.5:%d\n", data.pm25_env);
+  display.write(buff);
+  display.display();
+
+  for(int i=0; i<4; ++i){
+    digitalWrite(2+i, LOW);
+  }
+  if (data.pm100_env < 54) // good
+      digitalWrite(2, HIGH);
+  else if (data.pm100_env < 155) // moderate
+      digitalWrite(3, HIGH);
+  else if (data.pm100_env < 255) // bad for sensitive groups
+      digitalWrite(4, HIGH);
+  else
+      digitalWrite(5, HIGH);
+  delay(2000);
+ }
